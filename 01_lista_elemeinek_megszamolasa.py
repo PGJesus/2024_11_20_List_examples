cb95c4252af82ffadc98421f9160698b097ad5c6
MolnarHollo <molnar.hollo@diak.szbi-pg.hu>
2024-11-20 13:40:21 +0100

    add 01 & 02 task solution

--- 01_lista_elemeinek_megszamolasa.py
+++ 01_lista_elemeinek_megszamolasa.py
@@ -4,3 +4,13 @@ Határozd meg a lista hosszát egy ciklus segítségével
 (a len() függvény megoldásán kívül használj for ciklusos megoldást is),
 és írd ki!
 """
+
+szamok = [5, 8, 12, 15, 22]
+print(len(szamok))
+print()
+
+hossz = 0
+for szam in szamok:
+    hossz = hossz + 1
+
+print(f"A lista hossza: {hossz}")
